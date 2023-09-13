9dfc4391715566afde1cf378fac781e2ad41dcca
Karolina Groszewska <karolina.groszewska@gmail.com>
2023-09-12 22:07:16 -0500

    Final commit

--- app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
+++ app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
@@ -35,14 +35,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun configureImage() {
-        profileImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.ic_launcher_foreground))
+        profileImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.kari))
     }
 
     private fun configureName() {
         profileName.text = getString(
-            R.string.full_name,
-            getString(R.string.first_name),
-            getString(R.string.last_name)
+            R.string.full_name
         )
     }
 }
